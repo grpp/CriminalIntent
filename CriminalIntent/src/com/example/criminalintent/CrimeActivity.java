461a12882fcd8a0b09b5bb027f665c5f7da1bc26
grpp <nat_hanlaw@hotmail.com>
2014-08-04 15:27:16 -0400

    Save criminalintent project

---
+++ CriminalIntent/src/com/example/criminalintent/CrimeActivity.java
@@ -0,0 +1,11 @@
+package com.example.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+public class CrimeActivity extends SingleFragmentActivity {
+	@Override
+	protected Fragment createFragment(){
+		return new CrimeFragment();
+	}
+	
+}
