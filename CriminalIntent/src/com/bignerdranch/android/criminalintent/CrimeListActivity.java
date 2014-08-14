e6a6f99c459b0db41108de4e395ac8a5e6ec5ea2
grpp <nat_hanlaw@hotmail.com>
2014-08-13 22:41:02 -0400

    Chapter 9 and 10

--- CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeListActivity.java
+++ CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeListActivity.java
@@ -2,7 +2,7 @@ package com.bignerdranch.android.criminalintent;
 
 import android.support.v4.app.Fragment;
 
-public class CrimeActivity extends SingleFragmentActivity {
+public class CrimeListActivity extends SingleFragmentActivity {
 	
 	protected Fragment createFragment(){
 		return new CrimeListFragment();
